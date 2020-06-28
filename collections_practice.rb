9869b79c8840e085212ec38e38f6542ed71c2b0c
Michael R <mruhaak@gmail.com>
2020-06-28 09:31:00 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -32,3 +32,27 @@ def kesha_maker(array)
     item[2] = "$"
   end
 end
+
+def find_a(array)
+  array.find_all do |word|
+    word[0] == "a"
+  end
+end
+
+def sum_array(array)
+  sum = 0
+  array.each do |num|
+    sum+=num
+  end
+  sum
+end
+
+def add_s(array)
+  array.collect do |word|
+    if array[1] == word
+      word
+    else
+      word + "s"
+    end
+  end
+end
